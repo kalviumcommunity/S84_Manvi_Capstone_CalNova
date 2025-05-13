b13d889c894739d4db9971157ad87838de8d215c
Manvi027 <manvidadhwal@gmail.com>
2025-05-13 22:54:45 +0530

    database connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,29 @@
 const express=require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
+const userRoutes = require('./routes/user');
 
-const PORT =process.env.PORT || 8000;
-
-const appointmentRoutes = require('./routes/appointment');
-
+dotenv.config();
 app.use(express.json());
 
-app.use('/api',appointmentRoutes);
+const appointmentRoutes = require('./routes/appointment');
+const PORT =process.env.PORT || 8000;
 
-app.listen(PORT,()=>{
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log('MongoDB connected...!!');
+    app.listen(PORT,()=>{
     console.log(`server running at port http://localhost:${PORT}`);
+   });
 })
+.catch((err) => console.log('MongoDB connection error',err));
+
+app.use('/api',appointmentRoutes);
+app.use('/api/users',userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('API is running.');
+});
+
+
